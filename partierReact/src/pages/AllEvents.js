dfe890c05353019d8ad1852e8fb2f5a1f6bcf945
mateiocristi <mateiocristi@gmail.com>
2022-02-02 15:31:33 +0200

    added routes for getting events

--- partierReact/src/pages/AllEvents.js
+++ partierReact/src/pages/AllEvents.js
@@ -1,17 +1,52 @@
 import EventList from "../components/events/EventList";
+import { useEffect, useState } from "react";
 
 function AllEventsPage() {
-    const events = getEvents();
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedEvents, setLoadedEvents] = useState([]);
+
+    useEffect(async () => {
+        setIsLoading(true);
+        fetch(
+            "http://localhost:5000/events"
+        ).then(response => {
+            return response.json();
+        }).then(data => {
+            const events = [];
+            for (const key in data) {
+                const event = {
+                    id: key,
+                    ...data[key]
+                };
+                events.push(event);
+            }
+
+            setIsLoading(false);
+            setLoadedEvents(events);
+        });
+        // const response = await fetch("http://localhost:5000/events");
+        // const events = await response.json();
+    }, []);
+
+
+
+    if (isLoading) {
+        return (
+            <section>
+                <p>Loading...</p>
+            </section>
+        );
+    }
+
     return (
         <section>
-            <EventList events={events}/>
+            <EventList events={loadedEvents}/>
         </section>
     );
 }
 
-function getEvents() {
-    return require('../resources/events_dummy.json');
-}
+
 
 export default AllEventsPage;
 
