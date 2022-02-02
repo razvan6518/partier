7e287b24d61bb2fd9be1398c214f1f5303b030c5
mateiocristi <mateiocristi@gmail.com>
2022-02-02 12:08:30 +0200

    added spring application

---
+++ Partier/src/main/java/com/webCrawlers/Partier/PartierApplication.java
@@ -0,0 +1,13 @@
+package com.webCrawlers.Partier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartierApplication.class, args);
+	}
+
+}
