54adb0b27777262d88656849352edd22c13a0c25
mateiocristi <mateiocristi@gmail.com>
2022-02-15 13:33:40 +0200

    added date picker in user registration form, reformat RegisterForm component

--- client/src/components/layout/RegisterFrom.js
+++ client/src/components/layout/RegisterFrom.js
@@ -1,22 +1,38 @@
-import {useRef} from "react";
+import {useRef, useState} from "react";
 import Card from "../ui/Card";
 import classes from "./LoginFrom.module.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function RegisterForm(props) {
-    const usernameInputRef = useRef();
-    const passwordInputRef = useRef();
-    const emailInputRef = useRef();
-    const repeatPasswordInputRef = useRef();
+
+    // const firstNameInputRef = useRef();
+    // const lastNameInputRef = useRef();
+    // const emailInputRef = useRef();
+    // const passwordInputRef = useRef();
+    // const repeatPasswordInputRef = useRef();
+
+    const [password, setPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [username, setUsername] = useState("");
+
 
     function submitHandler(user) {
         user.preventDefault();
 
-        const enteredUsername = usernameInputRef.current.value;
-        const enteredPassword = passwordInputRef.current.value;
-        const enteredEmail = emailInputRef.current.value;
-        const enteredRepeatPassword = passwordInputRef.current.value;
 
-        const userItem = {"username": enteredUsername, "email": enteredEmail, "password": enteredPassword, "repeatedPassword": enteredRepeatPassword};
+
+        const userItem = {
+            "username": username,
+            "email": email,
+            "password": password,
+            "repeatedPassword": repeatPassword
+        };
+
 
         props.onRegisterUser(userItem);
     }
@@ -25,19 +41,31 @@ function RegisterForm(props) {
         <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.control}>
                 <label htmlFor="username">Username</label>
-                <input type="username" required id="username" ref={usernameInputRef}/>
+                <input type="username" required id="username" onChange={event => setUsername(event.target.value)}/>
             </div>
             <div className={classes.control}>
                 <label htmlFor="email">Email</label>
-                <input type="email" required id="email" ref={emailInputRef}/>
+                <input type="email" required id="email" onChange={event => setEmail(event.target.value)}/>
+            </div>
+            <div className={classes.control}>
+                <label htmlFor="firstName">First name</label>
+                <input type="firstName" required id="firstName" onChange={event => setFirstName(event.target.value)}/>
+            </div>
+            <div className={classes.control}>
+                <label htmlFor="lastName">First name</label>
+                <input type="lastName" required id="lastName" onChange={event => setLastName(event.target.value)}/>
+            </div>
+            <div className={classes.control}>
+                <label htmlFor="birthdate">Birthdate</label>
+                <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
             </div>
             <div className={classes.control}>
                 <label htmlFor="password">Password</label>
-                <input type="password" required id="password" ref={passwordInputRef}/>
+                <input type="password" required id="password" onChange={event => setPassword(event.target.value)}/>
             </div>
             <div className={classes.control}>
                 <label htmlFor="repeatPassword">Repeat Password</label>
-                <input type="Password" required id="repeatPassword" ref={repeatPasswordInputRef}/>
+                <input type="Password" required id="repeatPassword" onChange={event => setRepeatPassword(event.target.value)}/>
             </div>
             <div className={classes.actions}>
                 <button>Login</button>
